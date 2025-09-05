25ed4053716cc566ec5be449487fc87cee38a4e4
Piorunekszef <stargosz44@gmail.com>
2025-09-05 15:44:42 +0200

    Testowanie zmiennych Sucha

---
+++ liczby_całkowite.php
@@ -0,0 +1,15 @@
+<?php 
+// liczby całkowite 
+
+$liczba = 1234; // liczba dziesietna 
+$oktalna = 0123; // liczba osemkowa 
+$hex = 0x1A; // szesnastkowa
+$binary = 0b10101010; // binarna
+$big = 1_234_567; // duze liczby 
+echo $liczba . "<br>";
+print $oktalna . "<br>";
+echo $hex . "<br>" . $binary. "<br>";
+echo $big;
+
+
+?>
